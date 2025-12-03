acaf9327093400179a60d9716d4dbf4243e51c42
Owen Gutierrez <owga.gutierrez@gmail.com>
2025-12-03 15:45:09 +0800

    update admin

--- frontend/src/pages/Admin/components/pages/ManageUsers.jsx
+++ frontend/src/pages/Admin/components/pages/ManageUsers.jsx
@@ -87,7 +87,7 @@ function ManageUsers() {
   // Confirm delete
   const handleConfirmDelete = async () => {
     try {
-      const response = await fetch(`http://127.0.0.1:5000/api/admin/delete_user/${selectedId}`, {
+      const response = await fetch(`${API_URL}/api/admin/delete_user/${selectedId}`, {
           method: "DELETE",
           headers: { "Content-Type": "application/json" },
       });
