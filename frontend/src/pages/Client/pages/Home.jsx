cdc665ebdf68c73cd190efd0a191684bfe98b128
Owen Gutierrez <owga.gutierrez@gmail.com>
2025-12-03 15:16:37 +0800

    fix search bar

--- frontend/src/pages/Client/pages/Home.jsx
+++ frontend/src/pages/Client/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import logo from "@/assets/logo.png";
 import picture from "@/assets/landing page image.jpg";
